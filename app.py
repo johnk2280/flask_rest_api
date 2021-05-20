e7e7b6ae84394ea557a232aa3067062ce090cd87
johnk2280 <johnk2280@gmail.com>
2021-05-20 17:06:22 +0500

    [add] added GET and POST

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from flask_sqlalchemy import SQLAlchemy
+
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
 from config import Configuration
+
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 
 client = app.test_client()
 
-db = SQLAlchemy(app)
+engine = create_engine('sqlite:///db.sqlite')
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+from models import *
+
+Base.metadata.create_all(bind=engine)
 
 
 def create_short_url(url):
@@ -23,12 +37,31 @@ def check_url():
 
 @app.route('/test_task_api/v1.0', methods=['GET'])
 def get_url():
-    return 'Hello, Evgen'
+    urls = Urls.query.all()
+    urls_list = []
+    for url in urls:
+        urls_list.append({
+            'id': url.id,
+            'url': url.url,
+            'short_url': url.short_url,
+            'created_at': url.created_at,
+            'updated_at': url.updated_at
+        })
+
+    return jsonify(urls_list)
 
 
 @app.route('/test_task_api/v1.0', methods=['POST'])
 def insert_url():
-    pass
+    new_one = Urls(**request.json)
+    session.add(new_one)
+    session.commit()
+    url = {
+        'url': new_one.url,
+        'short_url': new_one.short_url
+    }
+
+    return jsonify(url)
 
 
 @app.route('/test_task_api/v1.0/<int:id>', methods=['PUT'])
@@ -39,3 +72,8 @@ def update_url():
 @app.route('/test_task_api/v1.0/<int:id>', methods=['DELETE'])
 def delete_url():
     pass
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    session.remove()
--- models.py
+++ models.py
@@ -1,12 +1,17 @@
 from app import db
+from app import session
+from app import Base
+
 from datetime import datetime
 
 
-class Urls(db.Model):
+class Urls(Base):
+    __tablename__ = 'urls'
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.Text, unique=True)
     short_url = db.Column(db.Text, unique=True)
     created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.now(), on_update=datetime.now())
 
-    def __init__(self, url, short_url):
-        super(Urls, self).__init__(self,)
+    # def __init__(self, url, short_url):
+    #     super(Urls, self).__init__(self,)
